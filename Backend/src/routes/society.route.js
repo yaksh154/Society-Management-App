3bd49ecc39012a488aced1c6f0ed60bb32ad7aad
srvraj111 <vraj.patel@swiftrut.com>
2024-10-25 21:37:50 +0530

    s

--- Backend/src/routes/society.route.js
+++ Backend/src/routes/society.route.js
@@ -5,6 +5,6 @@ const Society_Controller = require('../controllers/society.controller');
 router.post('/createsocieties', Society_Controller.createSociety);
 router.get('/societies/:id', Society_Controller.getSocietyById);
 
-// Add more routes if needed (e.g., PUT, DELETE)
+
 
 module.exports = router;
