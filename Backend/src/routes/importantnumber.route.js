9eb91571e9a310c455f457c718674ed242b39491
srvraj111 <vraj.patel@swiftrut.com>
2024-11-23 02:31:31 +0530

    auth

--- Backend/src/routes/importantnumber.route.js
+++ Backend/src/routes/importantnumber.route.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const importantnumber_controller = require("../controllers/importantnumber.controller");
 const { authUser } = require("../middleware/auth")
 
-router.post("/createImportantNumber", importantnumber_controller.createImportantNumber);
-router.get("/getAllImportantNumbers", importantnumber_controller.getAllImportantNumbers);
-router.put("/updateImportantNumber/:id", importantnumber_controller.updateImportantNumber);
-router.delete("/deleteImportantNumber/:id", importantnumber_controller.deleteImportantNumber);
+router.post("/createImportantNumber", authUser,importantnumber_controller.createImportantNumber);
+router.get("/getAllImportantNumbers", authUser,importantnumber_controller.getAllImportantNumbers);
+router.put("/updateImportantNumber/:id", authUser,importantnumber_controller.updateImportantNumber);
+router.delete("/deleteImportantNumber/:id", authUser,importantnumber_controller.deleteImportantNumber);
 
 module.exports = router;
