6ed5552b70e34ae8101bea2520cf337bfc4cc58a
srdarshan149 <darshan.vasita@swiftrut.com>
2024-11-24 00:27:18 +0530

    Security_protocols

--- frontend/src/component/pages/Resident/pages/Security_Protocols/Security_Protocols.jsx
+++ frontend/src/component/pages/Resident/pages/Security_Protocols/Security_Protocols.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../../layout/Sidebar'
 import Header from '../../layout/Header'
+import { FaUser } from 'react-icons/fa';
+import { Get_Security_Protocols } from '../../../../services/Api/api';
 
 const Security_Protocols = () => {
     let [data, setdata] = useState(280);
@@ -14,6 +16,17 @@ const Security_Protocols = () => {
         setdata(0);
         setget(0);
     }
+      
+
+  const [Security, setSecurity] = useState([])
+
+  useEffect(() => {
+      Fdata()
+  }, [])
+
+  const Fdata = () => {
+      Get_Security_Protocols(setSecurity)
+  }
     return (
         <div className='bg-[#f0f5fb] h-screen'>
             <Sidebar closeNav={closeNav} data={data} />
@@ -21,9 +34,33 @@ const Security_Protocols = () => {
                 <div className="open_he">
                     <Header openNav={openNav} />
                 </div>
-                <div className="">
-                Security_Protocols
-                </div>
+                <div className="p-6 rounded-lg ">
+          <div className="overflow-x-auto bg-white p-4 rounded-xl">
+            <table className="w-full table-auto">
+              <thead className="bg-blue-100 text-gray-700 ">
+                <tr>
+                  <th className="px-4 py-2 text-left">Title</th>
+                  <th className="px-4 py-2 text-left">Description</th>
+                  <th className="px-4 py-2 text-left">Date</th>
+                  <th className="px-4 py-2 text-left">Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Security.map((item, index) => (
+                  <tr key={index} className="border-b">
+                    <td className="px-4 py-3 flex items-center space-x-2">{item.Title }</td>
+                    <td className="px-4 py-3">{item.Description}</td>
+                    <td className="px-4 py-3">{item.Date}</td>
+
+                    <td className="px-4 py-3">
+                      <span className="px-2 py-1 rounded-full bg-gray-200 text-black">{item.Time}</span>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
             </div>
         </div>
     )
