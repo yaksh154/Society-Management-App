ad90a2193c3e3a67bbaf28ec939747cce4281b42
srvraj111 <vraj.patel@swiftrut.com>
2024-12-06 03:36:18 +0530

    String

--- Backend/src/models/OtherIncome.model.js
+++ Backend/src/models/OtherIncome.model.js
@@ -1,39 +1,39 @@
-const mongoose = require("mongoose");
+const  mongoose = require("mongoose");
 
 const otherincomeSchema = new mongoose.Schema(
-    {
-        Title: {
-            type: string,
-            require: true
-        },
-        Date: {
-            type: Date,
-            require: true
-        },
-        Due_Date: {
-            type: Date,
-            require: true
-        },
-        Description: {
-            type: string,
-            require: true
-        },
-        Amount: {
-            type: Number,
-            require: true
-        },
-        createdBy: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Manager'
-        },
-        Society: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Society'
-        }
+  {
+    Title: {
+        type: String,
+        require: true
     },
-    {
-        timestamps: true,
+    Date: {
+        type: Date,
+        require: true
+    },
+    Due_Date: {
+        type: Date,
+        require: true
+    },
+    Description: {
+        type: String,
+        require: true
+    },
+    Amount: {
+        type: Number,
+        require: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Manager'
+    },
+    Society: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Society'
     }
+  },
+  {
+    timestamps: true,
+  }
 );
 
 
