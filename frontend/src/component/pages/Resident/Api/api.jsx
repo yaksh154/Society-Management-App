86d3af280d88a56e26fb2b3811caab662a233ee7
srdarshan149 <darshan.vasita@swiftrut.com>
2024-12-06 00:05:46 +0530

    Update api.jsx

--- frontend/src/component/pages/Resident/Api/api.jsx
+++ frontend/src/component/pages/Resident/Api/api.jsx
@@ -149,8 +149,6 @@ export const PostOwnpoll = (data, Fdata, closeCreatePoll) => {
 
 
 
-
-
 export const GetNewPoll = async () => {
   const res = await axios.get('http://localhost:3030/OwnPoll');
   return res.data;
